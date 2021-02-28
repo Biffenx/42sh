26d1499fb48a831adc174e7ede3335e14a30bada
vkuokka <vkuokka@student.hive.fi>
2021-02-28 13:03:38 +0200

    Removed unnecessary variable declaration from lexer debug soure file.

--- src/lexer/debug.c
+++ src/lexer/debug.c
@@ -6,7 +6,7 @@
 /*   By: vkuokka <vkuokka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 17:10:44 by vkuokka           #+#    #+#             */
-/*   Updated: 2021/01/09 17:21:15 by vkuokka          ###   ########.fr       */
+/*   Updated: 2021/02/28 13:02:00 by vkuokka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	lexer_debug(t_lexer lexer)
 {
-	int		i;
-
-	i = 2;
 	ft_printf("%s: [%d]\n", "token count", lexer.count);
 	ft_printf("flags: ");
 	ft_print_bits(lexer.flags, 24);
@@ -28,6 +25,5 @@ void	lexer_debug(t_lexer lexer)
 		if (lexer.head == lexer.last)
 			break ;
 		lexer.head = lexer.head->next;
-		i++;
 	}
 }
