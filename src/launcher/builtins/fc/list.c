000c29d54161576fdd3aa17c48d34ab842fd6b75
vkuokka <vkuokka@student.hive.fi>
2021-03-25 17:15:49 +0200

    fc -l flag works with -r flag.

--- src/launcher/builtins/fc/list.c
+++ src/launcher/builtins/fc/list.c
@@ -6,7 +6,7 @@
 /*   By: vkuokka <vkuokka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 19:25:18 by vkuokka           #+#    #+#             */
-/*   Updated: 2021/03/14 20:01:12 by vkuokka          ###   ########.fr       */
+/*   Updated: 2021/03/25 17:09:33 by vkuokka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,26 @@ static void	latest(char options)
 	int		i;
 	int		count;
 
-	i = ft_arrlen(g_shell->history, HISTORY_SIZE) - 16;
-	i < 0 ? i = 0 : 0;
+	i = ft_arrlen(g_shell->history, HISTORY_SIZE) - 1;
 	count = 0;
-	while (i < HISTORY_SIZE && g_shell->history[i] && count < 16)
+	if (!(options & 1 << 3))
+	{
+		i -= 15;
+		i < 0 ? i = 0 : 0;
+		while (i < HISTORY_SIZE && g_shell->history[i] && count < 16)
+		{
+			options & 1 << 2 ? ft_putendl(g_shell->history[i])
+			: ft_printf("%-10d%s\n", i, g_shell->history[i]);
+			i += 1;
+			count += 1;
+		}
+		return ;
+	}
+	while (count < 16 && i >= 0)
 	{
 		options & 1 << 2 ? ft_putendl(g_shell->history[i])
 		: ft_printf("%-10d%s\n", i, g_shell->history[i]);
-		i += 1;
+		i -= 1;
 		count += 1;
 	}
 }
