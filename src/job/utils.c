6c0716caf9029774376319e523d3a443991fda5e
vkuokka <vkuokka@student.hive.fi>
2021-05-06 13:38:54 +0300

    Fixed bug causing jobs to be notified as stopped.

--- src/job/utils.c
+++ src/job/utils.c
@@ -6,7 +6,7 @@
 /*   By: vkuokka <vkuokka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 13:58:14 by vkuokka           #+#    #+#             */
-/*   Updated: 2021/05/06 11:03:41 by vkuokka          ###   ########.fr       */
+/*   Updated: 2021/05/06 13:26:33 by vkuokka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,10 @@ int				job_is_stopped (t_job *job)
 	while (process)
 	{
 		if (!process->completed && process->stopped)
-			return (0);
+			return (1);
 		process = process->next;
 	}
-	return 1;
+	return (0);
 }
 
 int				job_is_completed(t_job *job)
