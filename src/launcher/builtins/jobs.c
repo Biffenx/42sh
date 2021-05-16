389c323eefaa52596b1e5218841ce83365ba4cc6
vkuokka <vkuokka@student.hive.fi>
2021-05-16 11:25:05 +0300

    Jobs builtin almost aligns with the POSIX standard.

--- src/launcher/builtins/jobs.c
+++ src/launcher/builtins/jobs.c
@@ -6,7 +6,7 @@
 /*   By: vkuokka <vkuokka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 16:52:54 by vkuokka           #+#    #+#             */
-/*   Updated: 2021/05/15 15:25:30 by vkuokka          ###   ########.fr       */
+/*   Updated: 2021/05/16 11:22:40 by vkuokka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,100 @@
 ** https://www.man7.org/linux/man-pages/man1/jobs.1p.html
 */
 
+static int	parse_options(char **argv, int *i)
+{
+	int	options;
+	int	j;
+
+	options = 0;
+	j = 0;
+	while (argv[*i] && argv[*i][j] == '-')
+	{
+		while (argv[*i][j])
+		{
+			if (argv[*i][j + 1] == 'l')
+				options |= 1 << 0;
+			else if (argv[*i][j + 1] == 'p')
+				options |= 1 << 1;
+			j += 1;
+		}
+		*i += 1;
+	}
+	return (options);
+}
+
 static char	*state(t_job *job)
 {
 	if (job_is_completed(job))
-		return ("Completed");
+		return ("Done");
 	else if (job_is_stopped(job))
 		return ("Stopped");
 	else
 		return ("Running");
 }
 
-int jobs_builtin(char **argv)
+static void	list_all(int options)
 {
 	t_job	*job;
-	size_t	i;
+	int	job_id;
 
 	job = g_shell->jobs;
-	i = 1;
+	job_id = 0;
 	while (job)
 	{
-		if (ft_strequ(argv[1], "-l"))
-			ft_printf("[%i] %i %s %s\n", i, job->pgid, state(job), job->command);
-		else if (ft_strequ(argv[1], "-p"))
+		if (options & 1 << 0)
+			ft_printf("[%i] %i %s %s\n", job_id, job->pgid, state(job), job->command);
+		else if (options & 1 << 1)
 			ft_printf("%i\n", job->pgid);
 		else
-			ft_printf("[%i] %s %s\n", i, state(job), job->command);
+			ft_printf("[%i] %s %s\n", job_id, state(job), job->command);
 		job = job->next;
-		i += 1;
+		job_id += 1;
 	}
+}
+
+static void	list_selected(int options, char **argv, int *i)
+{
+	t_job	*job;
+	int	job_id;
+
+	while (argv[*i])
+	{
+		job = g_shell->jobs;
+		job_id = 0;
+		while (job)
+		{
+			if (job_id == ft_atoi(argv[*i]))
+			{
+				if (options & 1 << 0)
+					ft_printf("[%i] %i %s %s\n", job_id, job->pgid, state(job), job->command);
+				else if (options & 1 << 1)
+					ft_printf("%i\n", job->pgid);
+				else
+					ft_printf("[%i] %s %s\n", job_id, state(job), job->command);
+			}
+			job = job->next;
+			job_id += 1;
+		}
+		*i += 1;
+	}
+}
+
+int jobs_builtin(char **argv)
+{
+	int		options;
+	int		i;
+
+	i = 1;
+	options = parse_options(argv, &i);
+	if (g_debug)
+	{
+		ft_print_bits(options, 32);
+		ft_printf("\n%s\n", argv[i]);
+	}
+	if (!argv[i])
+		list_all(options);
+	else
+		list_selected(options, argv, &i);
 	return (0);
 }
