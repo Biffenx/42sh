b6f1152ec5b3ef80bcc0bd1262a6b0ba3189275a
vkuokka <vkuokka@student.hive.fi>
2021-07-30 12:37:50 +0300

    Made hash option parsing more clear.

--- src/launcher/builtins/hash.c
+++ src/launcher/builtins/hash.c
@@ -6,7 +6,7 @@
 /*   By: vkuokka <vkuokka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 09:37:58 by vkuokka           #+#    #+#             */
-/*   Updated: 2021/07/29 13:18:09 by vkuokka          ###   ########.fr       */
+/*   Updated: 2021/07/30 12:37:20 by vkuokka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,39 @@
 ** https://www.man7.org/linux/man-pages/man1/hash.1p.html
 */
 
-static int	parse_options(char ***argv)
+static void	set_option(char c, int *options)
 {
-	int		options;
-	size_t	i;
+	c == 'r' ? *options |= 1 << 0 : 0;
+	c == 0 ? *options |= 1 << 5 : 0;
+}
 
-	options = 0;
-	while (**argv && ***argv == '-' && ft_isalpha(*(**argv + 1)))
+static char	**parse_options(char **argv, int *options)
+{
+	size_t	i;
+	char	c;
+
+	*options = 0;
+	while (*argv && **argv == '-')
 	{
 		i = 1;
-		while ((**argv)[i] != '\0')
+		while ((*argv)[i])
 		{
-			if (!ft_strchr(HASHOPT, (**argv)[i]))
+			if (ft_strchr(HASHOPT, (*argv)[i]))
+				c = (*argv)[i];
+			else
+				c = 0;
+			set_option(c, options);
+			if (!c)
 			{
-				ft_dprintf(STDERR_FILENO, HASH_ERR_OPT, (**argv)[i]);
+				ft_dprintf(STDERR_FILENO, HASH_ERR_OPT, (*argv)[i]);
 				ft_dprintf(STDERR_FILENO, HASH_SYNTAX);
-				options |= 1 << 5;
-				return (options);
+				return (argv);
 			}
-			(**argv)[i] == 'r' ? options |= 1 << 0 : 0;
 			i += 1;
 		}
-		*argv += 1;
+		argv += 1;
 	}
-	return (options);
+	return (argv);
 }
 
 int		hash_builtin(char **argv)
@@ -50,15 +59,12 @@ int		hash_builtin(char **argv)
 
 	i = 0;
 	map = g_shell->paths;
-	options = parse_options(&argv);
+	parse_options(argv, &options);
 	if (options & 1 << 5)
 		return (1);
 	else if (options & 1 << 0)
-	{
 		hash_purge(map);
-		return (0);
-	}
-	if (!*argv)
+	else
 		while (i < HASH_SIZE)
 		{
 			if (map[i].key && map[i].value)
