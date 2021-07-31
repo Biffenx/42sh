3876f50f5fe3540126667cf429e21f12f98b2aa9
Don-Hege <heikki-seta@hotmail.com>
2021-08-01 00:02:10 +0300

    fixed 2> to be seen only as an error log

--- src/redirection/create.c
+++ src/redirection/create.c
@@ -6,7 +6,7 @@
 /*   By: hege <hege@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 18:01:30 by vkuokka           #+#    #+#             */
-/*   Updated: 2021/07/31 21:14:15 by hege             ###   ########.fr       */
+/*   Updated: 2021/07/31 23:59:54 by hege             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,13 @@ static void	write_heredoc(t_re_ag *node, t_token *token)
 	close(node->node.t_re.heredoc[1]);
 }
 
+static int is_error_redir(t_token *token)
+{
+	if (token->type & IO_NUM && ft_strequ(token->data, "2"))
+		return (1);
+	return (0);
+}
+
 static t_re_ag	*create_re_ag_node(t_token *token)
 {
 	t_re_ag	*new;
@@ -49,7 +56,7 @@ static t_re_ag	*create_re_ag_node(t_token *token)
 	new->e_flag = token->type & MASK_RE ? REDIR : AGGRE;
 	if (new->e_flag == REDIR)
 	{
-		new->node.t_re.redir = token->prev->type & IO_NUM ? 
+		new->node.t_re.redir = is_error_redir(token->prev) ? 
 		ft_strjoin(ft_strdup(token->prev->data), ft_strdup(token->data))
 		: ft_strdup(token->data);
 		new->node.t_re.file = ft_strdup(token->next->data);
